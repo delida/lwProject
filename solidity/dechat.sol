61f9c47b1e7a4c2da7a215651ee668eaad4a516b
delida <frank_1981@sina.com>
2018-11-06 17:37:17 +0800

    add dapp base function getRedeemMapping On branch master Changes to be committed: 	modified:   solidity/dechat.sol

--- solidity/dechat.sol
+++ solidity/dechat.sol
@@ -25,6 +25,33 @@ contract DappBase {
 	function DappBase() public payable {
 		owner = msg.sender;
 	}
+
+    function getRedeemMapping(address userAddr, uint start, uint end) public view returns (uint[] redeemingAmt, uint[] redeemingtime) {
+        uint i = 0;
+        uint j = 0;
+        uint k = 0;
+        
+        for (i = start; i <= end; i++) {
+            for (k = 0; k < redeem[i].userAddr.length; k++) {
+                if (redeem[i].userAddr[k] == userAddr) {
+                    j++;
+                }
+            }
+        }
+        uint[] memory amounts = new uint[](j);
+        uint[] memory times = new uint[](j);
+        j = 0;
+        for (i = start; i <= end; i++) {
+            for (k = 0; k < redeem[i].userAddr.length; k++) {
+                if (redeem[i].userAddr[k] == userAddr) {
+                    amounts[j] = redeem[i].userAmount[k];
+                    times[j] = redeem[i].time[k];
+                    j++;
+                }
+            }
+        }
+        return (amounts, times);
+    }
 	
 	function redeemFromMicroChain() public payable {//The user takes the coin to the main chain erc20
         redeem[block.number].userAddr.push(msg.sender);
